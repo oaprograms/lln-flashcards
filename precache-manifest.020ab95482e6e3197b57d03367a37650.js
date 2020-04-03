eefa86ca0833ab29ad061fe7634cdcf258d3ff24
Ognjen A <ognjen.apic@gmail.com>
2020-04-03 02:57:29 +0200

    initial

---
+++ precache-manifest.020ab95482e6e3197b57d03367a37650.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21089ddbeb7a10db1c15",
+    "url": "/css/app.f3372f6c.css"
+  },
+  {
+    "revision": "a5265d981a02d6ace5d7",
+    "url": "/css/chunk-vendors.e58dbb74.css"
+  },
+  {
+    "revision": "7691238f10a0b0c7cd7e73277b1430f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21089ddbeb7a10db1c15",
+    "url": "/js/app.d589797b.js"
+  },
+  {
+    "revision": "a5265d981a02d6ace5d7",
+    "url": "/js/chunk-vendors.e40a2990.js"
+  },
+  {
+    "revision": "5f7a4488f6353fdc00ef336a5eecbaec",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+importScripts("/precache-manifest.020ab95482e6e3197b57d03367a37650.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+//Web Push Notifications//
+// Import and configure the Firebase SDK
+importScripts('https://www.gstatic.com/firebasejs/7.13.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.13.1/firebase-messaging.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC457-gC0a0NqwoWhSrEp-VFgcVnBrKy3w",
+  authDomain: "nlle-b0128.firebaseapp.com",
+  databaseURL: "https://nlle-b0128.firebaseio.com",
+  projectId: "nlle-b0128",
+  storageBucket: "nlle-b0128.appspot.com",
+  messagingSenderId: "177081285221",
+  appId: "1:177081285221:web:1991e8331996687330a48c"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('Handling background message ', payload);
+  
+  return self.registration.showNotification(payload.data.title, {
+    body: payload.data.body,
+    icon: payload.data.icon,
+    tag: payload.data.tag,
+    data: payload.data.link
+  });
+});
